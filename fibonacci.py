bf2ca4532dd1ca78aa1395e8d550202d207d70ba
isjahputera <57501190+isjahputera@users.noreply.github.com>
2019-11-14 20:51:15 -0700

    created file that calculates the first 10 Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+# fibonacci.py
+
+f0 = int(1)
+f1 = int(1)
+f2 = f0 + f1
+f3 = f1 + f2
+f4 = f2 + f3
+f5 = f3 + f4
+f6 = f4 + f5
+f7 = f5 + f6
+f8 = f6 + f7
+f9 = f7 + f8
+
+print('OUTPUT', f0, f1, f2, f3, f4, f5, f6, f7, f8, f9)
